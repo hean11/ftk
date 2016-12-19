0e2dedf947d1ef99aa045dab3d9e7753a68be2f1
Robert Lehmann <robert.lehmann@sitec-systems.de>
2016-12-19 13:25:43 +0100

    Add README and License information
    Ticket #1608

---
+++ README.md
@@ -0,0 +1,14 @@
+# ftk -- Factory Toolkit
+
+## Introduction
+
+This tool supports the user to restore the hole systems image. It is although able
+to flash a new Kernel or DTB Image in a safe way. 
+
+## Usage
+
+For the usage please look at the help of the tool.
+
+```
+ftk -h
+```
--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
-# Setup for ftk
+# ftk - Managing flashing diffrent sections of the flash
+# Copyright (C) 2016  Robert Lehmann <robert.lehmann@sitec-systems.de>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 """setup.py setup for ftk"""
 
